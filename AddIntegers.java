2989123b78a1967e086357848e3f19d5be7b4a4e
hira sharif <hisharif@umich.edu>
2022-03-10 14:43:39 -0500

    added a new line

---
+++ AddIntegers.java
@@ -0,0 +1,14 @@
+public class AddIntegers {
+
+    public static void main(String[] args) {
+
+        int first = 15;
+        int second = 25;
+
+        System.out.println("Enter two numbers: " + first + " " + second);
+        int sum = first + second;
+
+        System.out.println("The sum is: " + sum);
+        System.out.println("This is hira. ");
+    }
+}
